c559ee1bc9a565fbebae3e9c511150a90052ec1e
Elvin <elvinhaxverdiyev777@gamil.com>
2025-02-16 18:59:11 +0400

    added correct url

--- mylibrary/mylibrary/urls.py
+++ mylibrary/mylibrary/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('apis/', include("apis.urls")),
+     path('api/v1/', include("apis.urls")),
 ]
